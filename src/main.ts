24ac32365f9a3f7fea81f63481f0ad0b17ea3854
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2020-05-01 18:53:50 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe, Logger } from '@nestjs/common';
+
+const PORT = 3000;
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(PORT);
+    Logger.log(`Application is running on port ${PORT}`);
+}
+
+bootstrap();
