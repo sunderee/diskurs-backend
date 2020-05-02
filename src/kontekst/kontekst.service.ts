8faa57b9c3a1d9b395381ebab99e85501829454a
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2020-05-02 11:30:30 +0200

    Use chaining and nullish coalescing

--- src/kontekst/kontekst.service.ts
+++ src/kontekst/kontekst.service.ts
@@ -48,27 +48,28 @@ export class KontekstService {
 
         const divs = $('#results > div > #fromserver').contents().text();
         const wordsMatch: RegExpMatchArray | null = divs.match(wordsRegex);
-        return wordsMatch != null
-            ? wordsMatch
-                  .filter((value: string) => value !== 'Fran' && value !== 'si ')
-                  .filter((value: string) => value !== 'lzmk' && value !== 'hr ')
-                  .filter((value: string) => value !== 'Vokabular' && value !== 'org')
-                  .reduce((previous: any[], _: string, index: number, array: string[]) => {
-                      if (index % 2 === 0) {
-                          previous.push(array.slice(index, index + 2));
-                      }
-                      return previous;
-                  }, [])
-                  .map(
-                      (array: string[]): KontekstResponseModel => ({
-                          word: array[0],
-                          similarity: parseInt(array[1], 10)
-                      })
-                  )
-            : ({
-                  error: 'Scraper issue',
-                  context: 'Yes',
-                  description: 'The returning RegExpMatchArray seems to be null'
-              } as ScrapingErrorModel);
+        return (
+            wordsMatch
+                ?.filter((value: string) => value !== 'Fran' && value !== 'si ')
+                .filter((value: string) => value !== 'lzmk' && value !== 'hr ')
+                .filter((value: string) => value !== 'Vokabular' && value !== 'org')
+                .reduce((previous: any[], _: string, index: number, array: string[]) => {
+                    if (index % 2 === 0) {
+                        previous.push(array.slice(index, index + 2));
+                    }
+                    return previous;
+                }, [])
+                .map(
+                    (array: string[]): KontekstResponseModel => ({
+                        word: array[0],
+                        similarity: parseInt(array[1], 10)
+                    })
+                ) ??
+            ({
+                error: 'Scraper issue',
+                context: 'Yes',
+                description: 'The returning RegExpMatchArray seems to be null'
+            } as ScrapingErrorModel)
+        );
     }
 }
